22c43272463aff306a0522283c99ccdfc0ded5d9
Ravindra Kamble <write2ravi14@gmail.com>
2025-08-27 21:50:11 +0530

    push

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import {connectDB} from './db/index.js';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+connectDB();
 app.listen(PORT,()=>{
     console.log(`Server started on port number... ${PORT}`);
 })
