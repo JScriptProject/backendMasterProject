26d9c497e900d394519d83ba19317b7ea509b285
Ravindra Kamble <write2ravi14@gmail.com>
2025-08-27 10:45:28 +0530

    db connected

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(`MongoDB connected !! DB HOST: ${connectionInstance}`);
+  } catch (error) {
+    console.error("MongoDb connection Error", error);
+    process.exit(1);
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`Server started on port number ${PORT}`);
+})
+
+
+/*
+//have taken the IIFE
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+  } catch (error) {
+    console.error("An Error occured while connecting to the database: ", error);
+    throw error;
+  }
+ app.listen(PORT,()=>{
+    console.log(`Server is listening on the port number ${process.env.PORT}`);
+  })
+  
+  app.on("error",(error)=>{
+    console.error("Issue while connecting to the server", error);
+    throw error;
+  })
+ 
+})();
+*/
